3887b02058c860b1a5362df863d5d904a495ad80
eddyTucubal <etucubal-2024505@kinal.edu.gt>
2025-08-15 18:41:25 -0600

    Errores arreglados

---
+++ src/org/jrae/controller/Ejercicios.java
@@ -0,0 +1,113 @@
+package org.jrae.controller;
+
+import org.jrae.view.MenuPrincipal;
+
+import java.util.Random;
+import java.util.Scanner;
+public class Ejercicios {
+    Scanner scanner = new Scanner(System.in);
+
+    public void ejercicio1Y2() {
+        String salida;
+            int numero;
+
+        do {
+            System.out.println("--------------------------------------------------");
+            System.out.println("Bienvenido a identificacion de numeros");
+            System.out.println("Ingrese un numero entero");
+            System.out.println("Ingrese 'SALIDA' para salir de la opcion");
+            System.out.println("--------------------------------------------------");
+            salida = scanner.nextLine();
+
+            if (salida.equalsIgnoreCase("SALIDA")){
+                break;
+            }
+            numero = Integer.parseInt(salida);
+
+        if (numero == 0) {
+            System.out.println("Numero neutro");
+        } else if (numero % 2 == 0) {
+            System.out.println("El numeor: "+numero+" es par");
+            if(numero == 2){
+                System.out.println("El numero: "+numero+" es primo");
+            } else if(numero > 2 && numero % 2 == 0){
+                System.out.println("El numero: "+numero+" no es primo");
+            } else{
+                System.out.println("El numero: "+numero+" es primo");
+            }
+        } else {
+            System.out.println("El numero: "+numero+" es impar");
+            if(numero == 2){
+                System.out.println("El numero: "+numero+" es primo");
+            } else if(numero > 2 && numero % 2 == 0){
+                System.out.println("El numero: "+numero+" no es primo");
+            } else{
+                System.out.println("El numero: "+numero+" es primo");
+            }
+        }
+
+        } while (true);
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        menuPrincipal.menuPrincipal();
+    }
+
+    public void ejercico3(){
+        String salida;
+        int numero;
+        int numeroAdivinar;
+        do {
+        Random random = new Random();
+            numeroAdivinar = random.nextInt(11);
+            System.out.println("--------------------------------------------------");
+            System.out.println("Bienvenido a adivina el numero");
+            System.out.println("--------------------------------------------------");
+            System.out.println("Ingresa un numero entre 0 y 10");
+            System.out.println("Ingrese 'SALIDA' para salir de la opcion");
+            System.out.println("--------------------------------------------------");
+            salida = scanner.nextLine();
+
+            if (salida.equalsIgnoreCase("SALIDA")){
+                break;
+            }
+            numero = Integer.parseInt(salida);
+        if (numero > 10 || numero < 0){
+            System.out.println("Numero fuera de parametros");
+        } else if (numero == numeroAdivinar){
+            System.out.println("Felicidades has adivinado el numero");
+        } else {
+            System.out.println("Perdiste el numero era: "+numeroAdivinar);
+        }
+
+        }while(true);
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        menuPrincipal.menuPrincipal();
+    }
+
+    public void ejercico4(){
+        String salida;
+        double celcios;
+        double resultado;
+        do {
+            System.out.println("--------------------------------------------------");
+            System.out.println("Bienvenido a Conversion de Centigrados a Fahrenheit");
+            System.out.println("--------------------------------------------------");
+            System.out.println("Ingresa un numero en grados");
+            System.out.println("Ingrese 'SALIDA' para salir de la opcion");
+            System.out.println("--------------------------------------------------");
+            salida = scanner.nextLine();
+
+            if (salida.equalsIgnoreCase("SALIDA")){
+                break;
+            }
+            celcios = Double.parseDouble(salida);
+
+            resultado = (celcios * 9/5)+32;
+            System.out.println("--------------------------------------------------");
+            System.out.println("El resultado en fahrenheit: "+resultado);
+            System.out.println("--------------------------------------------------");
+        }while(true);
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        menuPrincipal.menuPrincipal();
+    }
+
+}
--- src/org/jrae/view/MenuPrincipal.java
+++ src/org/jrae/view/MenuPrincipal.java
@@ -1,6 +1,6 @@
 package org.jrae.view;
 
-import org.jrae.controller.Ejercicio1;
+import org.jrae.controller.Ejercicios;
 
 import java.util.Scanner;
 
@@ -9,19 +9,40 @@ public class MenuPrincipal {
     public void menuPrincipal(){
 
         Scanner scanner = new Scanner(System.in);
+        String salida;
         int opcion;
         do{
+            System.out.println("Bienvenido al Menu Principal");
+            System.out.println("--------------------------------------------------");
             System.out.println("Elija una opcion");
-            System.out.println("1. Comprovacion de numero par o impar");
-         opcion = scanner.nextInt();
+            System.out.println("--------------------------------------------------");
+            System.out.println("1. Comprobacion de numeros");
+            System.out.println("2. Adivina el numero");
+            System.out.println("3. Convercion de Celcios a Fahrenheit");
+            System.out.println("Ingrese 'SALIDA' para salir de la opcion");
+            System.out.println("--------------------------------------------------");
+            salida = scanner.nextLine();
+
+            if (salida.equalsIgnoreCase("SALIDA")){
+                break;
+            }
+            opcion = Integer.parseInt(salida);
             switch(opcion){
             case 1:
-                Ejercicio1 ejercicio1 = new Ejercicio1();
-                ejercicio1.ejercicio1();
+                Ejercicios ejercicio1y2 = new Ejercicios();
+                ejercicio1y2.ejercicio1Y2();
                 break;
                 case 2:
+                    Ejercicios ejercicio3 = new Ejercicios();
+                    ejercicio3.ejercico3();
+                    break;
+                case 3:
+                    Ejercicios ejercicio4 = new Ejercicios();
+                    ejercicio4.ejercico4();
+                    break;
             }
-        } while(opcion == 0);
+        } while(true);
+        System.out.println("Ten un buen dia :)");
     }
 
 }
